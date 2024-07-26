4dd47e7c62eaa04412e5483686d7ceee53fba5fd
Lim Ding Wen <limdingwen@gmail.com>
2024-07-27 00:42:05 +0800

    In run script, make sure deployments happen before running the function

--- scripts/lib/functions/run.sh
+++ scripts/lib/functions/run.sh
@@ -1,3 +1,6 @@
 function run() {
+  echo "Waiting a while to make sure the deployment goes through..."
+  sleep 3
+  echo "Running function..."
   curl -L -X POST "https://$1.supabase.co/functions/v1/$3" -H "Authorization: Bearer $2" --data "$4"
 }
