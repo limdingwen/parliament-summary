518d01d44d05385bd4f38c952c54f187b5ccf5cd
Lim Ding Wen <limdingwen@gmail.com>
2024-07-28 09:46:03 +0800

    Make summary apology generic

--- site/src/app/components/SummaryNotAvailableApology.tsx
+++ site/src/app/components/SummaryNotAvailableApology.tsx
@@ -3,7 +3,7 @@ import { Text } from "@mantine/core";
 export default function SummaryNotAvailableApology() {
   return (
     <Text c="dimmed">
-      We&apos;re processing this bill&apos;s summary right now! Check back soon.
+      We&apos;re processing this summary right now! Check back soon.
     </Text>
   );
 }
