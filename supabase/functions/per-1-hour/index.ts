5f69268ec1c25a6a8631942cbfef212f131ece5e
Lim Ding Wen <limdingwen@gmail.com>
2024-07-28 10:20:57 +0800

    Schedule fillDebateSpeakerIds, fetchMpData and scrapeSittingReport

--- supabase/functions/per-1-hour/index.ts
+++ supabase/functions/per-1-hour/index.ts
@@ -1,7 +1,13 @@
 import "https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts";
 import { buildResponse } from "../lib/utils/build-response.ts";
 import scrapeBillsIntroduced from "../lib/shared-functions/scrape-bills-introduced.ts";
+import fetchMpData from "../lib/shared-functions/fetch-mp-data.ts";
+import scrapeSittingReport from "../lib/shared-functions/scrape-sitting-report.ts";
 
 Deno.serve(async (req) => {
-  return buildResponse([await scrapeBillsIntroduced(req)]);
+  return buildResponse([
+    await fetchMpData(req),
+    await scrapeSittingReport(req),
+    await scrapeBillsIntroduced(req),
+  ]);
 });
--- supabase/functions/per-10-min/index.ts
+++ supabase/functions/per-10-min/index.ts
@@ -2,9 +2,11 @@ import "https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts";
 import scrapeBillsPdf from "../lib/shared-functions/scrape-bills-pdf.ts";
 import generateBillsSummary from "../lib/shared-functions/generate-bills-summary.ts";
 import { buildResponse } from "../lib/utils/build-response.ts";
+import fillDebateSpeakerIds from "../lib/shared-functions/fill-debate-speaker-ids.ts";
 
 Deno.serve(async (req) => {
   return buildResponse([
+    await fillDebateSpeakerIds(req),
     await scrapeBillsPdf(req),
     await generateBillsSummary(req),
   ]);
