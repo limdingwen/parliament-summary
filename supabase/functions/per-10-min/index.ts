192c78e695c0ec3d661c7bfdd2d898b74bbcb1ed
Lim Ding Wen <limdingwen@gmail.com>
2024-08-07 04:11:00 +0800

    Re-enable fill debate bill IDs

--- supabase/functions/per-10-min/index.ts
+++ supabase/functions/per-10-min/index.ts
@@ -4,12 +4,12 @@ import generateBillsSummary from "../lib/shared-functions/generate-bills-summary
 import { buildResponse } from "../lib/utils/build-response.ts";
 // import fillDebateSpeakerIds from "../lib/shared-functions/fill-debate-speaker-ids.ts";
 import generateDebateSummary from "../lib/shared-functions/generate-debate-summary.ts";
-// import fillDebateBillIds from "../lib/shared-functions/fill-debate-bill-ids.ts";
+import fillDebateBillIds from "../lib/shared-functions/fill-debate-bill-ids.ts";
 
 Deno.serve(async (req) => {
   return buildResponse([
     // await fillDebateSpeakerIds(req),
-    // await fillDebateBillIds(req),
+    await fillDebateBillIds(req),
     await scrapeBillsPdf(req),
     await generateBillsSummary(req),
     await generateDebateSummary(req),
