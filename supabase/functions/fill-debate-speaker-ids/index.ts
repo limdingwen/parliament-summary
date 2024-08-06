133f6aa4fa888acb9092846cccc1c8098db8705b
Lim Ding Wen <limdingwen@gmail.com>
2024-08-07 04:35:32 +0800

    Remove fill [blank] functions, can be done using SQL views instead

--- supabase/functions/fill-debate-speaker-ids/index.ts
+++
@@ -1,5 +0,0 @@
-import "https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts";
-import proxyToResponseWrapper from "../lib/utils/proxy-to-response-wrapper.ts";
-import fillDebateSpeakerIds from "../lib/shared-functions/fill-debate-speaker-ids.ts";
-
-Deno.serve(proxyToResponseWrapper(fillDebateSpeakerIds));
--- supabase/functions/per-10-min/index.ts
+++ supabase/functions/per-10-min/index.ts
@@ -2,14 +2,10 @@ import "https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts";
 import scrapeBillsPdf from "../lib/shared-functions/scrape-bills-pdf.ts";
 import generateBillsSummary from "../lib/shared-functions/generate-bills-summary.ts";
 import { buildResponse } from "../lib/utils/build-response.ts";
-// import fillDebateSpeakerIds from "../lib/shared-functions/fill-debate-speaker-ids.ts";
 import generateDebateSummary from "../lib/shared-functions/generate-debate-summary.ts";
-import fillDebateBillIds from "../lib/shared-functions/fill-debate-bill-ids.ts";
 
 Deno.serve(async (req) => {
   return buildResponse([
-    // await fillDebateSpeakerIds(req),
-    await fillDebateBillIds(req),
     await scrapeBillsPdf(req),
     await generateBillsSummary(req),
     await generateDebateSummary(req),
