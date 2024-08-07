d7f2090f27c230651cae8a6208ec45c59eda6d80
Lim Ding Wen <limdingwen@gmail.com>
2024-08-07 18:26:32 +0800

    Schedule refresh debate bill match view

--- supabase/functions/per-10-min/index.ts
+++ supabase/functions/per-10-min/index.ts
@@ -3,9 +3,11 @@ import scrapeBillsPdf from "../lib/shared-functions/scrape-bills-pdf.ts";
 import generateBillsSummary from "../lib/shared-functions/generate-bills-summary.ts";
 import { buildResponse } from "../lib/utils/build-response.ts";
 import generateDebateSummary from "../lib/shared-functions/generate-debate-summary.ts";
+import refreshDebateBillMatchView from "../lib/shared-functions/refresh-debate-bill-match-view.ts";
 
 Deno.serve(async (req) => {
   return buildResponse([
+    await refreshDebateBillMatchView(req),
     await scrapeBillsPdf(req),
     await generateBillsSummary(req),
     await generateDebateSummary(req),
